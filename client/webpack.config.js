01c29d99b734b01999e88ddf3a57ad43b981c815
dustinii <125335987+dustinii@users.noreply.github.com>
2023-06-30 05:16:14 +0000

    added client and webpack.config.js template

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+
+    // TODO: Add the correct output
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+
+    // TODO: Add the correct plugins
+    plugins: [
+     new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+     }),
+    ],
+
+    // TODO: Add the correct modules
+    module: {
+
+    }
+  };
+};
